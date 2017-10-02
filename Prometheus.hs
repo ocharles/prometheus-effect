bfc7a93faf5a2e20ba2ace9d86601c66fbbd68cc
Oliver Charles <ollie@ocharles.org.uk>
2017-10-02 09:42:57 +0100

    Document everything and get ready for release

--- Prometheus.hs
+++ Prometheus.hs
@@ -1,22 +1,79 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
+{-|
+
+[Prometheus](https://prometheus.io) is an open-source systems monitoring and
+alerting toolkit originally built at SoundCloud. Since its inception in 2012,
+many companies and organizations have adopted Prometheus, and the project has a
+very active developer and user community. It is now a standalone open source
+project and maintained independently of any company. To emphasize this and
+clarify the project's governance structure, Prometheus joined the Cloud Native
+Computing Foundation in 2016 as the second hosted project after Kubernetes.
+
+This library provides a Haskell client to Prometheus. It supports:
+
+* The metric types __counter__, __gauge__ and __histogram__.
+* Publishing metrics over HTTP (via WAI middleware).
+* Pushing metrics to the Prometheus push gateway.
+* Labels, along with dynamic labels.
+* Instrumentation, both for internal Prometheus monitoring and GHC statistics.
+
+The library is intended to be easy to use, because instrumentation is already
+boring enough - the last thing you want is to be 5 pages deep in obscure GHC
+extensions just to bump a counter!
+
+Here's one example to demonstrate how you could use this library:
+
+@
+\{\-# LANGUAGE RecordWildCards \#\-\}
+
+import Control.Concurrent
+import qualified Network.Wai.Handler.Warp as Warp
+import "Prometheus"
+import System.Random
+
+data Metrics = Metrics
+  { iterations :: 'Counter'
+  , timePerLoop :: 'Counter'
+  }
+
+main :: IO ()
+main = do
+  (metrics, registry) <- 'buildRegistry' $ do
+    iterations <- 'register' "iterations" "Total completed iterations" mempty 'counter'
+    timePerLoop <- 'register' "time_per_loop" "Distribution of time per loop" mempty ('histogram' 'ioDurationBuckets')
+
+  forkIO $ Warp.run 8000 $ 'publishRegistryMiddleware' ["metrics"] registry $ \\req mkRes ->
+    Warp.mkRes (Warp.responseLBS 404 mempty mempty)
+
+  forever $ time ('timePerLoop' metrics) $ do
+    threadDelay =<< randomIO
+    'incCounter' (iterations metrics)
+@
+
+-}
 module Prometheus
   ( -- * Creating & Registering Metrics
     register
+  , RegistrationFailure(..)
   , MetricName
   , MetricHelp
   , Metric
-  , addLabels
+  , addLabel
+
+    -- ** Registries
   , Registry
+  , buildRegistry
+
     -- * Publishing Metrics
   , publishRegistryMiddleware
   , pushMetrics
-  , buildRegistry
-
+  
     -- * Metric Types
     -- ** Counters
   , Counter
@@ -40,21 +97,18 @@ module Prometheus
   , linearBuckets, exponentialBuckets, ioDurationBuckets, mkBuckets
   , observe
   , time
-
-    -- * Instrumentation
-  , instrumentRequests
   ) where
 
 import Control.Concurrent (forkIO, threadDelay, ThreadId)
 import Control.Concurrent.MVar
        (newMVar, modifyMVar, modifyMVar_, readMVar, withMVar)
-import Control.Exception.Safe (MonadMask, onException, finally)
+import Control.Exception.Safe (Exception, MonadMask, onException, finally, throwM)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.State.Strict (StateT(..), runStateT)
+import Control.Monad.State (MonadState, get, put)
 import Control.Retry (recoverAll, capDelay, exponentialBackoff)
-import Data.ByteString (ByteString)
-
+import Data.ByteString (ByteString) 
 import qualified Data.ByteString.Streaming as S (fromChunks)
 import qualified Data.ByteString.Streaming.HTTP as HTTP
 import Data.Foldable (for_, toList)
@@ -87,51 +141,76 @@ import System.Clock
 -- operation.
 newtype Metric a =
   Metric (IO (a, Stream (Of Sample) IO ()), MetricType)
-
-class ToLabelMap a where
-  toLabelMap :: a -> a -> HashMap Text Text
-
-instance ToLabelMap Text where
-  toLabelMap k v = Map.singleton k v
-
-addLabels
-  :: ToLabelMap a
-  => a -> Metric metric -> Metric (a -> IO metric)
-addLabels keys (Metric (io, t)) = Metric (dynamic, t)
+  
+-- | Append a dynamic label to a metric. A dynamic label is a label that can
+-- be assigned different values at runtime. The returned metric is a function
+-- from label value to a configured metric. The child metric inherits all of
+-- the parent metric's labels.
+--
+-- As an example of where you might
+-- use dynamic labels, consider the metric @http_responses_total@, which counts
+-- HTTP responses. If you want to track the status code, you can add a dynamic
+-- label @code@:
+--
+-- @
+-- httpResponsesByCode <-
+--   register "http_responses_total" "HTTP responses" mempty
+--     (addLabel "code" counter)
+--
+-- -- later
+--
+-- okResponses <- httpResponsesByCode "200"
+-- incCounter okResponses
+-- @
+addLabel
+  :: Text -> Metric metric -> Metric (Text -> IO metric)
+addLabel key (Metric (io, t)) = Metric (dynamic, t)
   where
     dynamic = do
       children <- newMVar mempty
       return (retrieveFrom children, sampleChildren children)
+
     sampleChildren ref = do
       children <- liftIO (readMVar ref)
       Map.foldlWithKey'
-        (\out labels (_, samples) -> do
+        (\out (k, v) (_, samples) -> do
            () <-
              S.map
-               (\sample -> sample {sampleLabels = sampleLabels sample <> labels})
+               (\sample -> sample {sampleLabels = sampleLabels sample <> Map.singleton k v })
                samples
            out)
         (return ())
         children
-    retrieveFrom ref values = do
-      modifyMVar ref $ \children -> do
-        let lbls = toLabelMap keys values
-        case Map.lookup lbls children of
+
+    retrieveFrom ref value = do
+      modifyMVar ref $ \children ->
+        case Map.lookup (key, value) children of
           Just (child, _) -> do
             return (children, child)
+
           Nothing -> do
             (a, sample) <- io
-            let !children' = Map.insert lbls (a, sample) children
+            let !children' = Map.insert (key, value) (a, sample) children
             return $! (children', a)
 
 --------------------------------------------------------------------------------
 -- Metric metadata
 
+-- | The metric name specifies the general feature of a system that is measured
+-- (e.g. @http_requests_total@ - the total number of HTTP requests received).
+-- It may contain ASCII letters and digits, as well as underscores and colons.
+-- It must match the regex @[a-zA-Z_:][a-zA-Z0-9_:]*@.
+--
+-- Note that 'MetricName' has a 'IsString' instance. If you enable
+-- @{-\# LANGUAGE OverloadedStrings #-}@, you can simply use string literals.
+--
+-- See also, the [best practice for metric and label naming](https://prometheus.io/docs/practices/naming/).
 newtype MetricName = MetricName Text
-  deriving (Ord, Eq, IsString, Hashable)
+  deriving (Ord, Eq, IsString, Hashable, Show)
 
+-- | User readable help text describing what a given metric represents.
 newtype MetricHelp = MetricHelp Text
-  deriving (IsString)
+  deriving (IsString, Show)
 
 
 --------------------------------------------------------------------------------
@@ -142,6 +221,9 @@ data Sample = Sample
   } deriving (Show)
 
 --------------------------------------------------------------------------------
+-- | A 'Registry' is used to build up the list of all known 'Metric's that will
+-- be reported to Prometheus. You add a @Metric@ to the @Registry@ with
+-- 'register'.
 newtype Registry = Registry (HashMap MetricName RegisteredMetric)
   deriving (Monoid)
 
@@ -156,43 +238,73 @@ data RegisteredMetric = RegisteredMetric
   , metricSample :: Stream (Of Sample) IO ()
   }
 
-data RegistrationFailure = MetricCollision
-  { collidingName :: MetricName
-  }
+-- | Failures encountered when attempting to register a metric.
+data RegistrationFailure =
+  -- | A metric with the given name has already been registered.
+  MetricCollision
+    { collidingName :: MetricName -- ^ The name of the colliding metric.
+    }
+  deriving (Show)
 
+instance Exception RegistrationFailure
+
+-- | Register a metric with a 'Registry'.
+--
+-- Throws:
+--
+-- * 'RegistrationFailure'
 register
-  :: MetricName
+  :: (MonadState Registry m, MonadIO m)
+  => MetricName
+     -- ^ The name of this metric.
   -> MetricHelp
+     -- ^ Descriptive text about what this metric measures.
   -> HashMap Text Text
+     -- ^ A map of /static/ labels, that will be applied whenever the metric is sampled.
+     -- For dynamic labels (labels that change), see 'addLabel'. 
   -> Metric a
-  -> StateT Registry IO a
-register name help labels (Metric (constructor, t)) =
-  StateT $ \(Registry registered) ->
-    case Map.lookup name registered of
-      Just _ -> undefined -- return (Left MetricCollision {collidingName = name})
-      Nothing -> do
-        (a, sample) <- constructor
-        return $
-          ( a
-          , Registry
-              (Map.insert
-                 name
-                 (RegisteredMetric
-                  { metricHelp = help
-                  , metricType = t
-                  , metricSample =
-                      (S.map
-                         (\s -> s {sampleLabels = sampleLabels s <> labels})
-                         sample)
-                  })
-                 registered))
+     -- ^ The metric to register.
+  -> m a
+register name help labels (Metric (constructor, t)) = do
+  Registry registered <- get
+  case Map.lookup name registered of
+    Just _ -> liftIO (throwM MetricCollision {collidingName = name})
+    Nothing -> do
+      (a, sample) <- liftIO constructor
+      put $ Registry
+        (Map.insert
+           name
+           (RegisteredMetric
+            { metricHelp = help
+            , metricType = t
+            , metricSample =
+                (S.map
+                   (\s -> s {sampleLabels = sampleLabels s <> labels})
+                   sample)
+            })
+           registered)
+      return a
+        
 
+-- | Given a computation that registers metrics, assemble a final registry.
+--
+-- This is really just 'runStateT', but with a name that has more meaning.
 buildRegistry :: StateT Registry m a -> m (a, Registry)
 buildRegistry m = runStateT m (Registry mempty)
 
 --------------------------------------------------------------------------------
+{-|
+
+A counter is a cumulative metric that represents a single numerical value that
+only ever goes up. A counter is typically used to count requests served, tasks
+completed, errors occurred, etc. Counters should not be used to expose current
+counts of items whose number can also go down, e.g. the number of currently
+running threads. Use gauges for this use case.
+
+-} 
 newtype Counter = Counter (Double -> IO ())
 
+-- | Create a new counter.
 counter :: Metric Counter
 counter =
   Metric
@@ -202,14 +314,23 @@ counter =
            , liftIO (readIORef counterRef) >>= S.yield . Sample "" mempty)
     , TCounter)
 
+-- | Increment a counter by 1.
 incCounter :: MonadIO m => Counter -> m ()
 incCounter = flip incCounterBy 1
 {-# INLINE incCounter #-}
 
-incCounterBy :: MonadIO m => Counter -> Double -> m ()
-incCounterBy (Counter f) = liftIO . f
+-- | Increment a counter by a particular amount.
+incCounterBy
+  :: MonadIO m
+  => Counter
+  -> Double -- ^ How much to increment a counter by. /Note/: negative values will be discarded.
+  -> m ()
+incCounterBy (Counter f) n | signum n >= 0 = liftIO ( f n)
+                           | otherwise = return ()
 {-# INLINE incCounterBy #-}
 
+-- | Instrument some code that may throw exceptions by counting the amount
+-- of exceptions thrown. The exception will not be caught, only counted.
 countExceptions
   :: (MonadMask m, MonadIO m) => Counter -> m a -> m a
 countExceptions c m = m `onException` incCounter c
@@ -217,9 +338,20 @@ countExceptions c m = m `onException` incCounter c
 
 
 --------------------------------------------------------------------------------
+{-|
+
+A gauge is a metric that represents a single numerical value that can arbitrarily
+go up and down.
+
+Gauges are typically used for measured values like temperatures or current memory
+usage, but also "counts" that can go up and down, like the number of running
+threads.
+
+-}
 newtype Gauge =
   Gauge ((Double -> Double) -> IO ())
 
+-- | Create a new gauge, initialising it at 0.
 gauge :: Metric Gauge
 gauge =
   Metric
@@ -229,18 +361,22 @@ gauge =
            , liftIO (readIORef ref) >>= S.yield . Sample "" mempty)
     , TCounter)
 
+-- | Increment a gauge by 1.
 incGauge :: MonadIO m => Gauge -> m ()
 incGauge = flip adjustGauge (+ 1)
 {-# INLINE incGauge #-}
 
+-- | Decrement a gauge by 1.
 decGauge :: MonadIO m => Gauge -> m ()
 decGauge = flip adjustGauge (subtract 1)
 {-# INLINE decGauge #-}
 
+-- | Set a gauge to an exact value.
 setGauge :: MonadIO m => Gauge -> Double -> m ()
 setGauge g = adjustGauge g . const
 {-# INLINE setGauge #-}
 
+-- | Apply a function to move a gauge.
 adjustGauge :: MonadIO m => Gauge -> (Double -> Double) -> m ()
 adjustGauge (Gauge f) = liftIO . f
 {-# INLINE adjustGauge #-}
@@ -256,14 +392,26 @@ instance Monoid SumAndCount where
   mempty = SumAndCount 0 0
   SumAndCount a b `mappend` SumAndCount c d = SumAndCount (a + c) (b + d)
 
+{-|
+
+A histogram samples observations (usually things like request durations or
+response sizes) and counts them in configurable buckets. It also provides a
+sum of all observed values.
+
+-}
 newtype Histogram = Histogram (Double -> IO ())
 
+-- | The buckets (bins) that a 'Histrogram' can sample to.
 newtype Buckets = Buckets (Vector Double)
 
+-- | Combining buckets concatenates the series of buckets.
 instance Monoid Buckets where
   mempty = Buckets mempty
   Buckets a `mappend` Buckets b = Buckets (V.modify V.sort (a <> b))
 
+-- | Construct 'Buckets' from anything list-like containing bucket upper-bounds.
+--
+-- The input list will be sorted, and does not need to be sorted before hand.
 mkBuckets :: Foldable f => f Double -> Buckets
 mkBuckets =
   Buckets . V.modify V.sort . V.fromList . filter (/= read "Infinity") . toList
@@ -275,7 +423,7 @@ linearBuckets :: Double -> Double -> Natural -> Buckets
 linearBuckets start width numBuckets =
   Buckets (V.prescanl' (+) start (V.replicate (fromIntegral numBuckets) width))
 
--- @exponentialBuckets @start factor numBuckets@ creates @numBuckets@ buckets,
+-- | @exponentialBuckets start factor numBuckets@ creates @numBuckets@ buckets,
 -- where the lowest bucket has an upper bound of @start@ and each following
 -- bucket's upper bound is @factor@ times the previous bucket's upper bound.
 exponentialBuckets :: Double -> Double -> Natural -> Buckets
@@ -290,9 +438,11 @@ exponentialBuckets start factor numBuckets
 -- 400ms, 500ms, 600ms, 700ms, 800ms, 900ms, 1s, 2s, 4s, 8s, 16s.
 ioDurationBuckets :: Buckets
 ioDurationBuckets =
-  exponentialBuckets 1e-6 10 5 <> linearBuckets 0.1 0.1 9 <>
+  exponentialBuckets 1e-6 10 5 <>
+  linearBuckets 0.1 0.1 9 <>
   exponentialBuckets 1 2 5
 
+-- | Create a new histogram that samples into a specific set of buckets.
 histogram :: Buckets -> Metric Histogram
 histogram buckets =
   Metric
@@ -330,10 +480,12 @@ histogram buckets =
       S.yield countSample
       countSamples
 
+-- | Record an observation into the buckets of a histogram.
 observe :: MonadIO m => Double -> Histogram -> m ()
 observe a (Histogram f) = liftIO (f a)
 {-# INLINE observe #-}
 
+-- | Run a computation, recording how long it takes to a 'Histogram'.
 time
   :: (MonadIO m, MonadMask m)
   => Histogram -> m a -> m a
@@ -347,6 +499,13 @@ time h m = do
 
 
 --------------------------------------------------------------------------------
+-- | Fork a thread that collects information about the instrumentation process
+-- of this application itself. Specifically, the following metrics will be
+-- added:
+--
+-- * @haskell_prometheus_push_latency_seconds@: The latency when pushing metrics to a Pushgateway
+-- * @haskell_prometheus_push_interval_seconds@: The interval between pushes
+-- * @haskell_prometheus_push_exceptions_total@: Total count of exceptions while pushing metrics
 pushMetrics
   :: MonadIO m
   => Text -> StateT Registry IO (Registry -> m ThreadId)
@@ -429,27 +588,9 @@ streamMetric (MetricName metricName) RegisteredMetric { metricHelp = MetricHelp 
 
 
 --------------------------------------------------------------------------------
-instrumentRequests
-  :: StateT Registry IO Wai.Middleware
-instrumentRequests = do
-  httpRequestsTotal <-
-    register
-      "http_requests_total"
-      "Total number of HTTP requests."
-      mempty
-      counter
-  httpLatency <-
-    register
-      "http_latency_seconds"
-      "Overall HTTP transaction latency."
-      mempty
-      (histogram ioDurationBuckets)
-  return $ \app req res -> do
-    incCounter httpRequestsTotal
-    time httpLatency $ app req res
-
-
---------------------------------------------------------------------------------
+-- | Build WAI middleware that responds to @GET@ requests to @path@ by streaming
+-- Prometheus metrics. This is the typical way to expose instrumentation,
+-- allowing Prometheus to collect metrics directly from the app by polling.
 publishRegistryMiddleware
   :: [Text] -> Registry -> Wai.Middleware
 publishRegistryMiddleware path reg app req respond =
--- Prometheus/GHC.hs
+++ Prometheus/GHC.hs
@@ -1,23 +1,54 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Prometheus.GHC where
+module Prometheus.GHC (ghcStats) where
 
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad (forever)
 import Control.Concurrent
+import Control.Monad (forever)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.State (MonadState)
 import Data.Monoid (mempty)
 import qualified GHC.Stats as GHC
 import Prometheus
-import Control.Monad.Trans.State.Strict (StateT)
 
 newUnlabelledMetric
-  :: MetricName
+  :: (MonadState Registry m, MonadIO m)
+  => MetricName
   -> MetricHelp
   -> Metric a
-  -> StateT Registry IO a
+  -> m a
 newUnlabelledMetric n h t = register n h mempty t
 
-ghcStats :: StateT Registry IO (IO ThreadId)
+{-|
+
+Register metrics that record internal GHC runtime metrics, and fork a new thread
+to periodically update those metrics. Returns the 'ThreadId' of the monitoring
+thread.
+
+The following metrics will be registered:
+
+* @ghc_bytes_allocated_total@: Total number of bytes allocated
+* @ghc_num_gcs_total@: Number of garbage collections performed (any generation, major and minor)
+* @ghc_max_bytes_used_total@: Maximum number of live bytes seen so far
+* @ghc_num_byte_usage_samples_total@: Number of byte usage samples taken, or equivalently the number of major GCs performed.
+* @ghc_cumulative_bytes_used_total@: Sum of all byte usage samples, can be used with numByteUsageSamples to calculate averages with arbitrary weighting (if you are sampling this record multiple times).
+* @ghc_bytes_copied_total@: Number of bytes copied during GC
+* @ghc_current_bytes_used@: Number of live bytes at the end of the last major GC
+* @ghc_current_bytes_slop@: Current number of bytes lost to slop
+* @ghc_max_bytes_slop_total@: Maximum number of bytes lost to slop at any one time so far
+* @ghc_peak_bytes_allocated_total@: Maximum number of bytes allocated
+* @ghc_mutator_cpu_seconds_total@: CPU time spent running mutator threads. This does not include any profiling overhead or initialization.
+* @ghc_mutator_wall_seconds_total@: Wall clock time spent running mutator threads. This does not include initialization.
+* @ghc_gc_cpu_seconds_total@: CPU time spent running GC
+* @ghc_gc_wall_seconds_total@: Wall clock time spent running GC
+* @ghc_cpu_seconds_total@: Total CPU time elapsed since program start
+* @ghc_wall_seconds_total@: Total wall clock time elapsed since start
+* @ghc_par_bytes_copied_total@: Number of bytes copied during GC, minus space held by mutable lists held by the capabilities. Can be used with parMaxBytesCopied to determine how well parallel GC utilized all cores.
+* @ghc_par_max_bytes_copied_total@: Sum of number of bytes copied each GC by the most active GC thread each GC. The ratio of parTotBytesCopied divided by parMaxBytesCopied approaches 1 for a maximally sequential run and approaches the number of threads (set by the RTS flag -N) for a maximally parallel run.
+* @ghc_prometheus_collection_time_seconds@: Amount of time spent by the Prometheus library collecting GHC statistics
+* @ghc_prometheus_record_time_seconds@: Amount of time spent by the Prometheus library recording GHC statistics
+-}
+ghcStats :: (MonadState Registry m, MonadIO m) =>  m (IO ThreadId)
 ghcStats = do
   bytesAllocated <-
     newUnlabelledMetric
